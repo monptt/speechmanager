6af1f3371343058c71084eeabf392a9166b5f079
pttkr <ptt_kr@outlook.jp>
2021-03-03 19:01:05 +0900

    stream continuing in while loop

--- AudioProcessing.py
+++ AudioProcessing.py
@@ -3,6 +3,16 @@ import numpy as np
 from PyQt5.QtCore import QThread, QObject, pyqtSignal
 import mora
 import copy
+import pyaudio
+
+SECOND = 1 # 何秒ごとに処理するか
+
+DEVICE_INDEX = -1   #default
+FORMAT = pyaudio.paInt16 # 16bit
+CHANNELS = 1             # monaural
+RATE = 16000           # sampling frequency [Hz]
+CHUNK = RATE * SECOND
+
 
 class AudioProcessingClass(QObject):
     updateSignal = pyqtSignal(np.ndarray, np.ndarray)
@@ -11,10 +21,23 @@ class AudioProcessingClass(QObject):
         super().__init__()
         self.mainWindow = main
 
+        ### PyAudioの設定 ###
+        self.p = pyaudio.PyAudio()
+        self.stream = self.p.open(format=FORMAT,
+                        channels=CHANNELS,
+                        rate=RATE,
+                        input=True,
+                        input_device_index = DEVICE_INDEX,
+                        frames_per_buffer=CHUNK)
+
     def run(self):
         y = np.zeros(10)
         while True:
-            moraNum, moraNumPerSec = mora.run()
+            frames = self.stream.read(CHUNK)
+            frames = b''.join([frames])
+            sampleBytes = self.p.get_sample_size(FORMAT)
+
+            moraNum, moraNumPerSec = mora.run(RATE, sampleBytes, frames, SECOND)
             #time.sleep(0.1)
             y = np.array([*y,moraNumPerSec][1:])
             x = np.arange(len(y))
@@ -22,3 +45,8 @@ class AudioProcessingClass(QObject):
             print(y)
 
             self.updateSignal.emit(x, y)
+
+        # 終了時(このままでは呼ばれない)
+        stream.stop_stream()
+        stream.close()
+        p.terminate()
